4fb8d7debb080b9969803b0a9af11b8115d24f0c
Salim Muthalib <m.s.s.muthalib@gmail.com>
2019-04-14 03:01:44 +0100

    increass the test coverage

--- tests/test_get_list_form_file.py
+++ tests/test_get_list_form_file.py
@@ -4,10 +4,12 @@ from .context import url_parser, db_utils
 DB_PATH = "./test_file.db"
 TABLE_NAME = "test_file"
 
+
 @pytest.fixture
 def db_update_connect():
     db_utils.db_terminate(DB_PATH)
     return db_utils.db_connection(DB_PATH)
+
 
 def test_io_error(db_update_connect):
     """ Testing if we get list of domain names
